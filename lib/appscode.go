a4154f00b0f56a2dae2a69dac9b2df12f0865756
sanjid133 <sanjid@appscode.com>
2017-11-20 13:22:24 +0600

    Remove expiration time for appscode token by using validation (#32)

--- lib/appscode.go
+++ lib/appscode.go
@@ -2,8 +2,12 @@ package lib
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"net/url"
+	"strings"
 
 	api "github.com/appscode/api/auth/v1beta1"
 	"github.com/appscode/api/dtypes"
@@ -11,6 +15,30 @@ import (
 	_env "github.com/appscode/go/env"
 	auth "k8s.io/api/authentication/v1beta1"
 )
+
+type WhoAmIResponse struct {
+	ErrorCode interface{}      `json:"error_code"`
+	ErrorInfo interface{}      `json:"error_info"`
+	Result    *ConduitUserData `json:"result"`
+}
+
+type ConduitUserData struct {
+	Image        string   `json:"image"`
+	Phid         string   `json:"phid"`
+	PrimaryEmail string   `json:"primaryEmail"`
+	RealName     string   `json:"realName"`
+	Roles        []string `json:"roles"`
+	URI          string   `json:"uri"`
+	UserName     string   `json:"userName"`
+}
+
+type ConduitClient struct {
+	Url  string
+	err  error
+	body []byte
+
+	Token string
+}
 
 func checkAppscode(name, token string) (auth.TokenReview, int) {
 	ctx := context.Background()
@@ -53,4 +81,38 @@ func checkAppscode(name, token string) (auth.TokenReview, int) {
 	data.Status.User.Groups = groups
 	data.Status.Authenticated = true
 	return data, http.StatusOK
+}
+
+func (p *ConduitClient) Call() *ConduitClient {
+	client := http.Client{}
+	form := url.Values{}
+	form.Add("api.token", p.Token)
+
+	phReq, err := http.NewRequest("POST", p.Url, strings.NewReader(form.Encode()))
+	if err != nil {
+		p.err = err
+		return p
+	}
+	phReq.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	phResp, err := client.Do(phReq)
+	if err != nil {
+		p.err = err
+		return p
+	}
+	message, err := ioutil.ReadAll(phResp.Body)
+	if err != nil {
+		p.err = err
+		return p
+	}
+	p.body = message
+	return p
+}
+
+func (p *ConduitClient) Into(i interface{}) error {
+	if p.err != nil {
+		return p.err
+	}
+
+	err := json.Unmarshal(p.body, i)
+	return err
 }
