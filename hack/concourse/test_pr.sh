cae8703149613a477819d00f1aee9e588839376e
Md. Tahsin Rahman <tahsynx@gmail.com>
2018-03-12 13:12:44 +0000

    Add concourse-ci pipeline (#87)

---
+++ hack/concourse/test_pr.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -x -e
+
+CURRENT_DIR=$(pwd)
+mkdir -p $GOPATH/src/github.com/appscode
+cp -r pull-request $GOPATH/src/github.com/appscode/guard
+cd $GOPATH/src/github.com/appscode/guard
+echo "testing commit"
+git rev-parse HEAD
+go build ./...
+go test -v ./...
