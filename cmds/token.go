1b71dfac8218644fa32d2869d52b0894efc2e9f3
nightfury1204 <nahid@appscode.com>
2018-03-20 18:04:49 +0600

    Add issue token for LDAP

--- cmds/token.go
+++ cmds/token.go
@@ -9,35 +9,59 @@ import (
 	"github.com/appscode/guard/github"
 	"github.com/appscode/guard/gitlab"
 	"github.com/appscode/guard/google"
+	"github.com/appscode/guard/ldap"
 	"github.com/appscode/guard/server"
 	"github.com/spf13/cobra"
 )
 
+type tokenOptions struct {
+	Org  string
+	LDAP ldap.TokenOptions
+}
+
 func NewCmdGetToken() *cobra.Command {
-	var org string
+	opts := tokenOptions{}
+
 	cmd := &cobra.Command{
 		Use:               "token",
 		Short:             fmt.Sprintf("Get tokens for %v", server.SupportedOrgPrintForm()),
 		DisableAutoGenTag: true,
 		Run: func(cmd *cobra.Command, args []string) {
-			org = strings.ToLower(org)
-			switch org {
+			opts.Org = strings.ToLower(opts.Org)
+			switch opts.Org {
 			case github.OrgType:
 				github.IssueToken()
+				return
 			case gitlab.OrgType:
 				gitlab.IssueToken()
+				return
 			case google.OrgType:
-				google.IssueToken()
+				err := google.IssueToken()
+				if err != nil {
+					log.Fatal(err)
+				}
+				return
 			case appscode.OrgType:
-				appscode.IssueToken()
+				err := appscode.IssueToken()
+				if err != nil {
+					log.Fatal(err)
+				}
+				return
+			case ldap.OrgType:
+				err := opts.LDAP.IssueToken()
+				if err != nil {
+					log.Fatal("For LDAP:", err)
+				}
+				return
 			case "":
 				log.Fatalln("Missing organization name. Set flag -o Google|Github.")
 			default:
-				log.Fatalf("Unknown organization %s.", org)
+				log.Fatalf("Unknown organization %s.", opts.Org)
 			}
 		},
 	}
 
-	cmd.Flags().StringVarP(&org, "organization", "o", org, fmt.Sprintf("Name of Organization (%v).", server.SupportedOrgPrintForm()))
+	cmd.Flags().StringVarP(&opts.Org, "organization", "o", opts.Org, fmt.Sprintf("Name of Organization (%v).", server.SupportedOrgPrintForm()))
+	opts.LDAP.AddFlags(cmd.Flags())
 	return cmd
 }
--- ldap/kubectl.go
+++ ldap/kubectl.go
@@ -3,32 +3,111 @@ package ldap
 import (
 	"encoding/base64"
 
+	"github.com/appscode/guard/cli"
 	"github.com/pkg/errors"
+	"github.com/spf13/pflag"
 	"gopkg.in/jcmturner/gokrb5.v4/client"
 	"gopkg.in/jcmturner/gokrb5.v4/crypto"
 	"gopkg.in/jcmturner/gokrb5.v4/messages"
 	"gopkg.in/jcmturner/gokrb5.v4/types"
+	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
 )
 
-func GetKerberosToken(userName, userPwd, realm, krb5Config, ServicePrincipalName string) (string, error) {
-	cl := client.NewClientWithPassword(userName, realm, userPwd)
+type TokenOptions struct {
+	Username string
 
-	c, err := cl.LoadConfig(krb5Config)
-	if err != nil {
-		return "", errors.Wrap(err, "failed to load krb5 config file")
-	}
+	UserPassword string
+
+	// set the realm to empty string to use the default realm from config
+	Realm string
+
+	Krb5configFile string
+
+	ServicePrincipalName string
 
 	// Active Directory does not commonly support FAST negotiation so you will need to disable this on the client.
 	// If this is the case you will see this error: KDC did not respond appropriately  to FAST negotiation To resolve
 	// this disable PA-FX-Fast on the client before performing Login()
-	c.GoKrb5Conf.DisablePAFXFast = true
+	DisablePAFXFast bool
+
+	// LDAP user authentication mechanism
+	// 0 for simple authentication
+	// 1 for kerberos(via GSSAPI)
+	// default: 0 (simple authentication)
+	AuthenticationChoice int
+}
+
+func (t *TokenOptions) AddFlags(fs *pflag.FlagSet) {
+	fs.StringVar(&t.Username, "ldap.username", t.Username, "Username")
+	fs.StringVar(&t.UserPassword, "ldap.password", t.UserPassword, "Password")
+	fs.StringVar(&t.Realm, "ldap.realm", t.Realm, "Realm, set the realm to empty string to use the default realm from config")
+	fs.StringVar(&t.Krb5configFile, "ldap.krb5-config", "/etc/krb5.conf", "Path to the kerberos configuration file")
+	fs.StringVar(&t.ServicePrincipalName, "ldap.spn", t.ServicePrincipalName, "Service principal name")
+	fs.BoolVar(&t.DisablePAFXFast, "ldap.disable-pa-fx-fast", true, "Disable PA-FX-Fast, Active Directory does not commonly support FAST negotiation so you will need to disable this on the client")
+	fs.IntVar(&t.AuthenticationChoice, "ldap.auth-choice", 0, "LDAP user authentication mechanism, 0 for simple authentication, 1 for kerberos(via GSSAPI)")
+}
+
+func (t *TokenOptions) IssueToken() error {
+	var (
+		token string
+		err   error
+	)
+
+	err = t.Validate()
+	if err != nil {
+		return err
+	}
+
+	switch t.AuthenticationChoice {
+	case 0:
+		token = t.getSimpleAuthToken()
+		break
+	case 1:
+		token, err = t.getKerberosToken()
+		if err != nil {
+			return err
+		}
+		break
+	default:
+		return errors.New("invalid authentication choice")
+	}
+
+	return t.addAuthInfo(token)
+}
+
+func (t *TokenOptions) Validate() error {
+	if t.Username == "" {
+		return errors.New("username is required")
+	}
+	if t.UserPassword == "" {
+		return errors.New("password is required")
+	}
+	if t.AuthenticationChoice == 1 && t.ServicePrincipalName == "" {
+		return errors.New("service principal is required")
+	}
+	return nil
+}
+
+func (t *TokenOptions) getSimpleAuthToken() string {
+	return base64.StdEncoding.EncodeToString([]byte(t.Username + ":" + t.UserPassword))
+}
+
+func (t *TokenOptions) getKerberosToken() (string, error) {
+	cl := client.NewClientWithPassword(t.Username, t.Realm, t.UserPassword)
+
+	c, err := cl.LoadConfig(t.Krb5configFile)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to load krb5 config file")
+	}
+
+	c.GoKrb5Conf.DisablePAFXFast = t.DisablePAFXFast
 
 	err = c.Login()
 	if err != nil {
 		return "", errors.Wrap(err, "login unsuccessful")
 	}
 
-	tkt, key, err := c.GetServiceTicket(ServicePrincipalName)
+	tkt, key, err := c.GetServiceTicket(t.ServicePrincipalName)
 	if err != nil {
 		return "", errors.Wrap(err, "failed to get service ticket")
 	}
@@ -58,4 +137,12 @@ func GetKerberosToken(userName, userPwd, realm, krb5Config, ServicePrincipalName
 		return "", errors.Wrap(err, "failed to marshal AP_REQ message")
 	}
 	return base64.StdEncoding.EncodeToString(data), nil
+}
+
+func (t *TokenOptions) addAuthInfo(token string) error {
+	authInfo := &clientcmdapi.AuthInfo{
+		Token: token,
+	}
+
+	return cli.AddAuthInfoToKubeConfig(t.Username, authInfo)
 }
