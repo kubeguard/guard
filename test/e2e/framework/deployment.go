b45ab679f3bafe258a8655dd712a0287c224bf41
Md. Nure Alam Nahid <nahid@appscode.com>
2018-06-21 19:16:34 +0600

    Fix test (#159)

--- test/e2e/framework/deployment.go
+++ test/e2e/framework/deployment.go
@@ -1,5 +1,14 @@
 package framework
 
+import (
+	apps "k8s.io/api/apps/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
 func (f *Framework) DeleteDeployment(name, namespace string) error {
 	return f.KubeClient.AppsV1beta1().Deployments(namespace).Delete(name, deleteInBackground())
+}
+
+func (f *Framework) GetDeployment(name, namespace string) (*apps.Deployment, error) {
+	return f.KubeClient.AppsV1beta1().Deployments(namespace).Get(name, metav1.GetOptions{})
 }
