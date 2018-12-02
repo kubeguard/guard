a54f4f7ed3988797f7903096f917df4cd5db3ff4
Tamal Saha <tamal@appscode.com>
2018-12-01 23:13:01 -0800

    Set periodic analytics
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- commands/root.go
+++ commands/root.go
@@ -3,26 +3,17 @@ package commands
 import (
 	"flag"
 	"log"
-	"strings"
 
 	v "github.com/appscode/go/version"
-	"github.com/appscode/kutil/tools/analytics"
-	"github.com/jpillora/go-ogle-analytics"
+	"github.com/appscode/kutil/tools/cli"
 	"github.com/json-iterator/go"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 )
 
-const (
-	gaTrackingCode = "UA-62096468-20"
-)
-
 var json = jsoniter.ConfigCompatibleWithStandardLibrary
 
 func NewRootCmd() *cobra.Command {
-	var (
-		enableAnalytics = true
-	)
 	cmd := &cobra.Command{
 		Use:                "guard [command]",
 		Short:              `Guard by AppsCode - Kubernetes Authentication WebHook Server`,
@@ -32,19 +23,13 @@ func NewRootCmd() *cobra.Command {
 			c.Flags().VisitAll(func(flag *pflag.Flag) {
 				log.Printf("FLAG: --%s=%q", flag.Name, flag.Value)
 			})
-			if enableAnalytics && gaTrackingCode != "" {
-				if client, err := ga.NewClient(gaTrackingCode); err == nil {
-					client.ClientID(analytics.ClientID())
-					parts := strings.Split(c.CommandPath(), " ")
-					client.Send(ga.NewEvent(parts[0], strings.Join(parts[1:], "/")).Label(v.Version.Version))
-				}
-			}
+			cli.SendAnalytics(c, v.Version.Version)
 		},
 	}
 	cmd.PersistentFlags().AddGoFlagSet(flag.CommandLine)
 	// ref: https://github.com/kubernetes/kubernetes/issues/17162#issuecomment-225596212
 	flag.CommandLine.Parse([]string{})
-	cmd.PersistentFlags().BoolVar(&enableAnalytics, "analytics", enableAnalytics, "Send analytical events to Google Guard")
+	cmd.PersistentFlags().BoolVar(&cli.EnableAnalytics, "analytics", cli.EnableAnalytics, "Send analytical events to Google Guard")
 
 	cmd.AddCommand(NewCmdInit())
 	cmd.AddCommand(NewCmdGet())
