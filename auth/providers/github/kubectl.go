3b7aac5f123d1b15a03a4392ad074e722a3d8d70
Noah Kantrowitz <noah@coderanger.net>
2018-12-30 16:55:26 -0700

    Add a little bit more guidance for users.

--- auth/providers/github/kubectl.go
+++ auth/providers/github/kubectl.go
@@ -8,5 +8,8 @@ import (
 func IssueToken() {
 	codeURurl := "https://github.com/settings/tokens/new"
 	term.Infoln("Github url for personal access tokens:", codeURurl)
+	term.Infoln("Select only read:org permissions for the token")
+	term.Infoln("After the token is created, run:")
+	term.Infoln("    kubectl config set-credentials <user_name> --token=<token>")
 	open.Start(codeURurl)
 }
