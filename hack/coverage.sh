b8ae05a42166538493d1c7d12d44eef7bd015cdd
Md. Nure Alam Nahid <knightnahid@gmail.com>
2019-06-12 15:03:13 +0600

    Fix LDAP test (#223)

--- hack/coverage.sh
+++ hack/coverage.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -eou pipefail
 
+export CGO_ENABLED=0
+export GO111MODULE=on
+export GOFLAGS="-mod=vendor"
+
 GOPATH=$(go env GOPATH)
 REPO_ROOT="$GOPATH/src/github.com/appscode/guard"
 
@@ -9,7 +13,7 @@ pushd $REPO_ROOT
 echo "" >coverage.txt
 
 for d in $(go list ./... | grep -v -e vendor -e test); do
-  go test -v -race -coverprofile=profile.out -covermode=atomic "$d"
+  go test -installsuffix "static" -v -coverprofile=profile.out -covermode=atomic "$d"
   if [ -f profile.out ]; then
     cat profile.out >>coverage.txt
     rm profile.out
