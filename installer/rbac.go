b1984adb57be5a557cf10469ce0f3f4444090dc9
David Schneider <schneider@puzzle.ch>
2023-06-28 13:58:49 +0200

    Update RBAC API version
    Signed-off-by: David Schneider <schneider@puzzle.ch>

--- installer/rbac.go
+++ installer/rbac.go
@@ -18,7 +18,7 @@ package installer
 
 import (
 	core "k8s.io/api/core/v1"
-	rbac "k8s.io/api/rbac/v1beta1"
+	rbac "k8s.io/api/rbac/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 )
