f8d89b838ff2d49c5ed28706e615d4397ca9df1b
Mark Drobnak <markdronak@microsoft.com>
2026-04-23 10:51:09 -0700

    Fix test after recent change
    Signed-off-by: Mark Drobnak <markdronak@microsoft.com>

--- installer/deployment_test.go
+++ installer/deployment_test.go
@@ -440,7 +440,7 @@ func assertEntraSDKProxyCertInitContainer(t *testing.T, initContainer core.Conta
 		[]string{
 			"sh",
 			"-c",
-			"mkdir -p /etc/pki/ca-trust/extracted/openssl /etc/pki/ca-trust/extracted/pem /etc/pki/ca-trust/extracted/java /etc/pki/ca-trust/extracted/edk2 && update-ca-trust",
+			"mkdir -p /etc/pki/ca-trust/extracted/{openssl,pem,java,edk2} && update-ca-trust",
 		},
 		initContainer.Command,
 	)
